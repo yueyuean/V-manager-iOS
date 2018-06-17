bd403e7aa880f33ebbf3ba6c2245da13d0a76056
laa <aa.li@tamigroup.com>
2018-06-17 11:04:04 +0800

    第一次提交

---
+++ v/v/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  v
+//
+//  Created by 塔米 on 2018/6/17.
+//  Copyright © 2018年 tamigroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ v/v/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  v
+//
+//  Created by 塔米 on 2018/6/17.
+//  Copyright © 2018年 tamigroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
